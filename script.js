49d482b5377483a08369b5542435b259568a0286
Lalit Kumar Verma <lalit2506verma@gmail.com>
2025-04-27 23:55:32 +0530

    change logic to highlight and possible moves

--- script.js
+++ script.js
@@ -9,50 +9,210 @@ const arrayOfFiles = Array.from(squares);
 arrayOfFiles.forEach(file => {
 
     //counter
-    let counter = 1;
+    let counter = 8;
 
     Array.from(file.children).forEach(item => {
 
         item.setAttribute("id", filesName[fileCounter] + counter)
-        counter++;
+        counter--;
     })
 
     fileCounter++;
 
 })
 
-const allSquare = document.querySelectorAll('.square');
-const clickedElements = [];
+const allSquares = document.querySelectorAll('.square');
+let clickedElements = [];
 
-Array.from(allSquare).forEach(square => {
 
-    square.addEventListener("click", () => {
+// Handle clicks
+Array.from(allSquares).forEach(square => {
 
-        const idOfElement = square.getAttribute("id");
-        // get inner html
-        const element = document.getElementById(idOfElement);
-        const innerHTMLOfElement = element.innerHTML;
+    square.addEventListener("click", (e) => {
 
-        if (innerHTMLOfElement.includes("black") || innerHTMLOfElement.includes("white")) {
-            // unclick any other element
-            unclickedElements();
-            // change the color of the square
-            element.style.backgroundColor = '#f5f681';
-            console.log("element clicked");
+        let clickedElement = e.target;
+        const parentSquare = clickedElement.parentElement;
 
+        if (clickedElement.tagName === 'DIV') {
+            clickedElement = clickedElement.querySelector('IMG');
         }
 
-        // push element in the array
-        clickedElements.push(square);
+        if (clickedElement && clickedElement.tagName === 'IMG') {
+            console.log('You clicked:', clickedElement.dataset.color, clickedElement.dataset.piece);
+
+            const color = clickedElement.dataset.color;
+            const pieceType = clickedElement.dataset.piece;
+            const currPosition = parentSquare.getAttribute('id');
+
+            clearHighlights();
+            removeHighlightedMove();
+
+            // Separating black and white color
+            if (color === 'black') {
+                // Handle Black pieces
+                switch (pieceType) {
+
+                    case 'pawn':
+
+                        highlightSquare(clickedElement);
+                        highlightPawnMoves(currPosition, color);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            else {
+                // white
+                switch (pieceType) {
+                    case 'pawn':
+
+                        highlightSquare(clickedElement);
+                        highlightPawnMoves(currPosition, color);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        //const idOfElement = square.getAttribute("id");
+        // get inner html
+        // const element = document.getElementById(idOfElement);
+        // const innerHTMLOfElement = element.innerHTML;
+
+        // if (innerHTMLOfElement.includes("black") || innerHTMLOfElement.includes("white")) {
+        //     // unclick any other element
+        //     //unclickedElements();
+        //     // change the color of the square
+        //     element.style.backgroundColor = '#baca2c';
+        //     console.log("element clicked");
+
+        // }
+
+        // // push element in the array
+        // clickedElements.push(square);
 
     })
 })
 
-function unclickedElements() {
+// Reset another selected sqaure
+function clearHighlights() {
+    clickedElements.forEach(square => {
+        square.classList.remove('highlighted-square');
+    });
+    clickedElements = [];
+}
 
-    if (clickedElements.length > 0) {
+function highlightSquare(clickedElement) {
+    clickedElement.classList.add('highlighted-square');
+    clickedElements.push(clickedElement);
+}
 
-        clickedElements[0].removeAttribute("style");
-        clickedElements.pop();
+function highlightPawnMoves(pos, color) {
+    console.log(pos);
+
+    const file = pos[0];
+    const rank = parseInt(pos[1]);
+
+    let nextRank = color === 'white' ? rank + 1 : rank - 1;
+    let doubleNextRank = color === 'white' ? rank + 2 : rank - 2;
+
+    highlightMove(file + nextRank);
+
+    // Allow double move only if on starting position
+    if (color === 'white' && rank === 2 || color === 'black' && rank === 7) {
+        highlightMove(file + doubleNextRank);
     }
 }
+
+// Global array to track highlighted move
+let highlightMoves = [];
+
+// Highlighting next Possible Moves
+function highlightMove(position) {
+
+    const square = document.querySelector("#" + position);
+    if (square) {
+        const circle = document.createElement('div');
+        circle.classList.add('highlighted-circle');
+        square.appendChild(circle);
+        highlightMoves.push(square);
+    }
+
+}
+
+// Remove existing Highlighted move
+function removeHighlightedMove() {
+
+    highlightMoves.forEach(square => {
+        const child = square.querySelector('.highlighted-circle');
+
+        child.remove();
+    })
+
+    highlightMoves = [];
+}
+
+// const arrayOfPawn = [];
+
+// // Black Pawn laws
+// filesName.forEach(file => {
+//     arrayOfPawn.push(document.getElementById(file + 2));
+// })
+
+// // White Pawn laws
+// filesName.forEach(file => {
+//     arrayOfPawn.push(document.getElementById(file + 7));
+// })
+
+// arrayOfPawn.forEach(pawn => {
+//     pawn.addEventListener('click', () => {
+//         const pawnCurrID = pawn.getAttribute("id");
+
+//         console.log(pawn);
+//         const pieceColor = pawn.dataset;
+//         // const type = pawn.dataset.piece
+//         let currRank = parseInt(pawnCurrID[1]);
+//         console.log(pieceColor);
+
+
+//         const attachCircles = [];
+
+//         for (let i = 1; i <= 2; i++) {
+//             currRank++;
+//             attachCircles.push(document.getElementById(pawnCurrID[0] + currRank));
+//         }
+
+//         highlightSquare(attachCircles);
+//     })
+// })
+
+// // ARRAY TO TRACK HIGHLIGHTED CIRCLES
+// const trackCircles = [];
+
+// // FUNCTION TO HIGHLIGHT POSSIBLE STEPS
+// const highlightSquare = function (attachCircles) {
+
+//     // Remove old circles
+//     if (trackCircles.length > 0) {
+//         trackCircles.forEach(parent => {
+//             const children = parent.querySelectorAll('.highlighted-circle');
+//             children.forEach(child => child.remove());
+//         });
+//         // After removing all children, clear the tracking array
+//         trackCircles.length = 0;
+//     }
+
+//     const child = `<div class="highlighted-circle"> </div>`;
+
+//     // Add new circles
+//     attachCircles.forEach(el => {
+//         el.insertAdjacentHTML('beforeend', child); // safer than innerHTML
+//         trackCircles.push(el);
+//     });
+
+//     console.log(trackCircles);
+// };
